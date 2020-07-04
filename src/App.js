f97d7cdb4829a67a4d78c17f812f3aa5cf7a427d
anhquoc1104 <anhquoc1104@gmail.com>
2020-07-04 11:55:53 +0700

    searchBar component & verify typing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './components/TodoList'
 import TodoList from './components/TodoList';
 import Login from './components/Login';
 import Menu from './components/Menu';
+// import { Component } from 'react';
 
-function App() {
-  let menu = [
-    {icon: "fas fa-home", label: 'HOME'},
-    {icon: "fas fa-gift", label: 'DEALS'},
-    {icon: "fas fa-cloud-upload-alt", label: 'UPLOAD'},
-    {icon: "fas fa-mug-hot", label: 'WORK'},
-    {icon: "fas fa-cog", label: 'SETTINGS'}
-  ];
-  let hasUnread = true;
-  return (
-    <div className="App">
-      {/* <header className="App-header"></header> */}
-      {/* <Login email="amin@gmail.com"/> */}
-      <Menu menu={menu} hasUnread={hasUnread} />
-      {/* <TodoList /> */}
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: [
+        { icon: "fas fa-home", label: 'HOME' },
+        { icon: "fas fa-gift", label: 'DEALS' },
+        { icon: "fas fa-cloud-upload-alt", label: 'UPLOAD' },
+        { icon: "fas fa-mug-hot", label: 'WORK' },
+        { icon: "fas fa-cog", label: 'SETTINGS' }
+      ],
+      hasUnread: true,
+      isfocused: false
+    }
+  };
+  focusedChange = () => {
+    if (this.state.isfocused) this.setState({ isfocused: false });
+    else this.setState({ isfocused: true });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* <header className="App-header"></header> */}
+        {/* <Login email="amin@gmail.com"/> */}
+        <Menu
+          menu={this.state.menu}
+          hasUnread={this.state.hasUnread}
+          focusInput={this.state.isfocused}
+          focusFn={this.focusedChange}
+        />
+        {/* <TodoList /> */}
+      </div>
+    );
+  };
+};
 
 export default App;
--- src/components/menu.js
+++ src/components/menu.js
@@ -2,10 +2,56 @@ import React, { Component } from 'react';
 import './Menu.css'
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      styleInputWrong: {
+        borderColor: "#f00",
+        outline: "0",
+        boxShadow: "0 0 0 0.2rem rgba(255,0,0,.25)"
+      },
+      styleInputTrue: {}
+    }
+  };
+
+  changeFocus = () => {
+    this.props.focusFn();
+  };
+
+  setStyleWrong = (elm) => {
+    if (elm) {
+      return this.setState({
+        styleInputTrue: this.state.styleInputWrong
+      });
+    }else{
+      return this.setState({
+        styleInputTrue: {}
+      });
+    }
+  };
+
+  onChecked = (e) => {
+    let value = e.target.value;
+    let arrValue = value.split('');
+    // let value = e.target.value.split('');
+    let arrNum = [0, 9, 0];
+    //check length = 10
+    if (value.length > 10 || isNaN(value)) return this.setStyleWrong(1);
+    for (let i = 0; i < value.length; i++) {
+      if (i > 2) break;
+      if (Number(arrValue[i]) !== arrNum[i]) return this.setStyleWrong(1);
+    };
+    return this.setStyleWrong(0);
+  };
+
   render() {
-    let {menu, hasUnread } = this.props;
-    let classIcon = "far fa-bell"
-    if(hasUnread) classIcon += " icon__notify";
+    let { menu, hasUnread, focusInput } = this.props;
+    let classIconNotify = "far fa-bell";
+    let classIconSearch = "icon__search rounded-circle";
+
+    if (hasUnread) classIconNotify += " icon__notify";
+    if (focusInput) classIconSearch += " hidden";
+
     return (
       <div className="header__menu container-fluid p-2">
         <div className="row">
@@ -21,9 +67,25 @@ class Menu extends Component {
               })
             }
           </div>
+          <div className="col">
+            <input
+              id="input-search"
+              className="input__search form-control rounded-pill"
+              type="text"
+              placeholder="Type something to search..."
+              onFocus={this.changeFocus}
+              onBlur={this.changeFocus}
+              onChange={this.onChecked}
+              style={this.state.styleInputTrue}
+            />
+            <div className={classIconSearch}>
+              <i className="fas fa-search text-white" />
+            </div>
+          </div>
+
           <div className="col d-flex justify-content-end">
             <div className="header__notify pt-1 mr-5">
-              <i className={classIcon}></i>
+              <i className={classIconNotify}></i>
             </div>
           </div>
         </div>
